fda4b170f5af773bfd159b96cb79c8df61fdcce7
Yeong-Ung Kim <yeongungkim@gm.gist.ac.kr>
2025-12-03 20:38:50 +0900

    update

--- BridgeEx/src/bridge_des.cpp
+++ BridgeEx/src/bridge_des.cpp
@@ -1,28 +1,41 @@
 #include <thread>
+#include <string>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 class Bridge : public rclcpp::Node {
     public:
-        Bridge(): Node("Bridge") {
+        Bridge(const std::string & nodeName, const rclcpp::NodeOptions & options): Node(nodeName, options) {
             RCLCPP_INFO(this->get_logger(), "--- Start ---");
         }
 
-        void setup(const rclcpp::Node::SharedPtr& src, const rclcpp::Node::SharedPtr& des){
-            _subFsrc = src->create_subscription<std_msgs::msg::String>("/String", 10, bind(&Bridge::subCallback, this, std::placeholders::_1));
-
+        void setup(const rclcpp::Node::SharedPtr & des){
+            // outward
+            _subFsrc = this->create_subscription<std_msgs::msg::String>("/Sub/String", 10, bind(&Bridge::subCallback, this, std::placeholders::_1));
             _pub2des = des->create_publisher<std_msgs::msg::String>("/String", 10);
+
+            // inward
+            _subFdes = des->create_subscription<std_msgs::msg::String>("/String", 10, bind(&Bridge::pubCallback, this, std::placeholders::_1));
+            _pub2src = this->create_publisher<std_msgs::msg::String>("/Pub/String", 10);
         }
 
-        void subCallback(const std_msgs::msg::String& msg){
+        void subCallback(const std_msgs::msg::String & msg){
             _pub2des->publish(msg);
             RCLCPP_INFO(this->get_logger(), "Receive a msg from [src]; Pub to [des]");
+            RCLCPP_INFO(this->get_logger(), "%s", msg.data.c_str());
+        }
+
+        void pubCallback(const std_msgs::msg::String & msg){
+            _pub2src->publish(msg);
+            RCLCPP_INFO(this->get_logger(), "Receive a msg from [des]; Pub to [src]");
             RCLCPP_INFO(this->get_logger(), "%s", msg.data.c_str());
         }
 
     private:
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub2des;
         rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subFsrc;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub2src;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subFdes;
 };
 
 int main(int argc, char* argv[]){
@@ -32,23 +45,24 @@ int main(int argc, char* argv[]){
     src_context->init(argc, argv);
     rclcpp::NodeOptions srcOption = rclcpp::NodeOptions();
     srcOption.context(src_context);
-    auto src = rclcpp::Node::make_shared("CurrentDomain", srcOption);
+    // auto src = rclcpp::Node::make_shared("CurrentDomain", srcOption);
+    auto bridge = std::make_shared<Bridge>("Bridge", srcOption);
 
+    uint8_t domain = 99;
     rclcpp::InitOptions options;
-    options.set_domain_id(99);
+    options.set_domain_id(domain);
     auto des_context = std::make_shared<rclcpp::Context>();
     des_context->init(argc, argv, options);
     rclcpp::NodeOptions desOption = rclcpp::NodeOptions();
     desOption.context(des_context);
-    auto des = rclcpp::Node::make_shared("Domain99", desOption);
+    auto des = rclcpp::Node::make_shared("Bridge" + std::to_string(domain), desOption);
 
-    auto bridge = Bridge();
-    bridge.setup(src, des);
+    bridge->setup(des);
 
     auto src_exec = std::make_shared<rclcpp::executors::StaticSingleThreadedExecutor>();
     auto des_exec = std::make_shared<rclcpp::executors::StaticSingleThreadedExecutor>();
 
-    src_exec->add_node(src);
+    src_exec->add_node(bridge);
     des_exec->add_node(des);
 
     std::thread threadSrc([=] {src_exec->spin(); } );
